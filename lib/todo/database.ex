053b6e5efb3de2ddb3f721648fe2c6f12decc750
Matthew B. Reisch <matt.reisch@gmail.com>
2017-02-28 12:54:29 -0500

    Refactored to allow for supervised databaseworkers

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -1,10 +1,10 @@
 defmodule Todo.Database do
   use GenServer
 
-  @db_workers 3
-  
+  @pool_size 3
+
   def start_link(db_folder) do
-    GenServer.start_link(__MODULE__, db_folder, name: :database_server)
+    Todo.PoolSupervisor.start_link(db_folder, @pool_size)
   end
 
   def store(key, data) do
@@ -20,24 +20,6 @@ defmodule Todo.Database do
   end
 
   defp choose_worker(key) do
-    GenServer.call(:database_server, {:choose_worker, key})
+    :erlang.phash2(key, @pool_size) + 1
   end
-
-  def init(db_folder) do
-    IO.puts "Starting database server"
-    {:ok, start_workers(db_folder)}
-  end
-
-  defp start_workers(db_folder) do
-    Enum.map(1..@db_workers, fn(index) ->
-      {:ok, pid} = Todo.DatabaseWorker.start_link(db_folder)
-      {index - 1, pid}
-    end)
-    |> Enum.into(Map.new)
-  end
-  
-  def handle_call({:choose_worker, key}, _, workers) do
-    worker_key = :erlang.phash2(key, @db_workers)
-    {:reply, Map.get(workers, worker_key), workers}
-  end
-end
+ end
